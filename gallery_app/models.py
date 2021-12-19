b54091c0252366d269a2952a581e6e198c1b0c51
Farhan Islam <farhanislam@Farhans-MacBook-Pro.local>
2021-12-19 15:41:09 +0600

    Added Perks on Picture model

--- gallery_app/models.py
+++ gallery_app/models.py
@@ -25,7 +25,11 @@ class Picture(models.Model):
     image = models.ImageField(upload_to='images/')
     title = models.CharField(max_length=100, unique=True)
     description = models.CharField(max_length=1000, null=True, blank=True)
+    perks = models.IntegerField(default=0)
     created = models.DateTimeField(default = timezone.now)
+
+    class Meta:
+        ordering = ['-perks']
 
     def __str__(self):
         return self.title + ' from ' + self.album.title
